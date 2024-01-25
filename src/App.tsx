0aec019bb93aaf2918200d663ed9f024473be575
zetavg <mail@zeta.vg>
2024-01-26 04:10:14 +0800

    add Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 import logo from './logo.svg';
 
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="bg-slate-800 min-h-screen flex flex-col items-center justify-center text-[calc(10px_+_2vmin)] text-white">
+        <img
+          src={logo}
+          className="h-[40vmin] motion-safe:animate-[spin_20s_linear_infinite] pointer-events-none"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-[#61dafb]"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const config = {
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  important: 'body',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
